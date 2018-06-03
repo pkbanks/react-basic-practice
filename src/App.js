778f5675607a065b2c5df777dc835af2a7f5b23e
PK Banks <pkbanks12@gmail.com>
2018-06-03 15:26:47 -0400

    got basic features to work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 
+class Notification extends Component {
 
-
-function App() {
-
-  render() {
-    // TODO: Pass Notification its props
-    return (
-      <div id="app">
-        <Notification message={this.props.message} type={this.props.type} />
-      </div>
-    )
+  getClassType = () => {
+    let result = "";
+    switch(this.props.notification["type"]) {
+      case "success":
+        result = "alert alert-success";
+        break;
+      case "message":
+        result = "alert alert-info";
+        break;
+      case "caution":
+        result = "alert alert-warning";
+        break;
+      case "error":
+        result = "alert alert-danger";
+        break;
+      default:
+        result = "alert alert-info";
+    }
+    return result;
   }
+  
+  render() {
+    const msg = this.props.notification['message'];
+    const classType = ( msg == "" ? "" : this.getClassType() )
+    return (
+      <div className={classType}>
+        {msg}
+      </div>
+    );
+  }
+}
+
+function App(props) {
+
+  // TODO: Pass Notification its props
+  return (
+    <div id="app">
+      <Notification notification={props.notification} />
+    </div>
+  )
 }
 
 export default App;
